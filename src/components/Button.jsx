2250e1ffb10f0fb94d018518db12ed70448574f3
Camila Delgado <camiladelgado.1321@gmail.com>
2024-04-21 06:16:34 -0400

    feature(common): added question-2

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
+import { useState } from "react"
 import "../styles/buttonStyles.css"
 
 export const Button = props => {
     
-    const { text } = props;
+    const { text } = props
     
-    const[count, setCount] = useState(5);
+    const[count, setCount] = useState(5)
     
     function handlerButton() {
-        setCount(count + 50);
+        setCount(count + 50)
     }
     
     return (
         <button onClick={ handlerButton }>
             { text } { count }
         </button>
-    );
+    )
 }
